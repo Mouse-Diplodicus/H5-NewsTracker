9aa3417c285ee9d9de8f1a683a7ccf5da1338238
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2020-02-26 19:09:53 -0700

    Renamed Primary working directory due to the structure requirements put forth by the client.

---
+++ H5_News_Tracker/hello.py
@@ -0,0 +1,12 @@
+"""
+high level support for doing this and that.
+"""
+import tkinter
+
+Root = tkinter.Tk()
+
+Label = tkinter.Label(Root, text="Hi, nice to see you")
+
+Label.pack()
+
+Root.mainloop()
--- src/hello.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk, Label
-
-root = Tk()
-
-label = Label(root, text="Hi, nice to see you")
-
-label.pack()
-
-root.mainloop()
