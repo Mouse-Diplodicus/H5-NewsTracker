dfe71f20d4d62e2da2936561dd800d2178f3b56d
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2020-03-02 16:12:38 -0700

    added some themeing options

--- H5_News_Tracker/hello.py
+++ H5_News_Tracker/hello.py
@@ -2,13 +2,20 @@
 Program displays a window with text using Tkinter when run.
 """
 import tkinter
+from tkinter import ttk
 
 
 def main():
     """Displays a window onscreen with the text, 'Hi, nice to see you!'"""
     root = tkinter.Tk()
-    label = tkinter.Label(root, text="Hi, nice to see you!")
-    label.pack()
+    root.overrideredirect(1)
+    style = ttk.Style()
+    style.configure("WB.TLabel", foreground="#ffffff", background="#000000")
+    style.configure("R.TLabel", foreground="#000000", background="#931113")
+    label = ttk.Label(root, text="Hi, nice to see you!", style="WB.TLabel")
+    button_exit = ttk.Button(root, text="X", padding=[4, 0, 4, 0], style="R.TLabel", command=root.quit)
+    label.grid(row=0, column=0)
+    button_exit.grid(row=0, column=1)
     root.mainloop()
 
 
