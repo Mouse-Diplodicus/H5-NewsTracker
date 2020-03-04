79348dd0bc5e48df716fead3324b965f381883b5
Tj Virbick <tjvirb@gmail.com>
2020-03-04 11:09:22 -0700

    Started unittesting

---
+++ H5_News_Tracker/test_HyperLink.py
@@ -0,0 +1,17 @@
+import unittest
+import HyperLink
+
+
+class TestHyperLink(unittest.TestCase):
+    def test_mock_reddit_link(self):
+        pass
+    
+    def test_resize(self):
+        pass
+
+    def test_open_RSS_Ticker(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
