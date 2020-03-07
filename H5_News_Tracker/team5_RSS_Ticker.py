a843520fdafab0e3a9e7f7a963f311d66152e33e
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2020-03-07 12:46:29 -0700

    fixing Linting errors

--- H5_News_Tracker/team5_RSS_Ticker.py
+++ H5_News_Tracker/team5_RSS_Ticker.py
@@ -5,8 +5,8 @@ import tkinter as tk
 import webbrowser
 
 
-def display_RSS_Ticker():
-    """Displays a window onscreen with a reddit link'"""
+def display_rss_ticker():
+    """ Displays a window onscreen with a reddit link'"""
     root = tk.Tk()
     link1 = tk.Label(root, text="Reddit Hyperlink", fg="black", cursor="hand2")
     link1.bind("<Button-1>", lambda e: callback('http://www.reddit.com/'))
@@ -14,8 +14,11 @@ def display_RSS_Ticker():
 
     root.mainloop()
 
+
 def callback(url):
+    """ Opens new tab displaying the url that is provided"""
     webbrowser.open_new(url)
 
+
 if __name__ == "__main__":
-    display_RSS_Ticker()
+    display_rss_ticker()
