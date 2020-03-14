ce6b68fb7c98e0974ee700f532654e3acedeec1c
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2020-03-14 12:20:27 -0600

    Initial Push

---
+++ H5_News_Tracker/controller/main.py
@@ -0,0 +1,24 @@
+"""
+Reused code on StackOverFlow
+https://stackoverflow.com/questions/49362820/update-rss-feed-every-2-minutes
+"""
+import feedparser
+import threading
+from H5_News_Tracker.gui import ticker_window
+
+
+def main(**kw):
+    """Uses ticker_window to show the google news feed"""
+    print("importing and parsing rss feed")
+    google_news = feedparser.parse('https://news.google.com/news/rss')
+    # feed_show = {'entries': [{feed['entries'][0]['title']}]}
+    news_ticker = ticker_window.TickerWindow()
+    news_ticker.update(google_news.feed.title, google_news.feed.link)
+    gui_update_thread = threading.Thread(target=news_ticker.start(), name="GUI-Main-Thread")
+    gui_update_thread.start()
+    print("when do we get here?")
+
+
+if __name__ == '__main__':
+    print("Program starting")
+    main()
---
+++ H5_News_Tracker/gui/ticker_window.py
@@ -0,0 +1,49 @@
+"""
+Program displays a window with text using Tkinter when run.
+"""
+import tkinter
+import webbrowser
+from tkinter import ttk
+
+
+class TickerWindow:
+
+    root = tkinter.Tk()
+    label_ticker = ttk.Label(root)
+    button_exit = ttk.Button(root)
+
+    def __init__(self, master=None, **kw):
+        """Initializes the display window for the news ticker"""
+        print("constructing gui")
+        self.root.overrideredirect(1)
+        self.label_ticker.configure(width=8)
+        self.button_exit.configure(text="X", padding=[4, 0, 4, 0], command=self.root.quit)
+        self.set_style()
+        self.build()
+        print("Gui constructed")
+
+    def start(self):
+        """Start gui main update loop"""
+        print("starting main loop")
+        self.root.mainloop()
+
+    def set_style(self):
+        """Sets styling for various Tkinter objects"""
+        print("setting styling")
+        style = ttk.Style()
+        style.configure("default.TLabel", foreground="#000000", background="#ffffff")
+        style.configure("WB.TLabel", foreground="#ffffff", background="#000000")
+        style.configure("BR.TLabel", foreground="#000000", background="#931113")
+        self.label_ticker.configure(style="WB.TLabel")
+        self.button_exit.configure(style="BR.TLabel")
+
+    def build(self):
+        """Sets organization for label and exit button"""
+        print("organizing gui layout")
+        self.label_ticker.grid(row=0, column=0)
+        self.button_exit.grid(row=0, column=1)
+
+    def update(self, headline, url):
+        print("updating ticker to headline: ", headline, "   url: ", url)
+        self.label_ticker.configure(text=headline, width=len(headline))
+        self.label_ticker.bind("<Button-1>", lambda e: webbrowser.open_new(url))
