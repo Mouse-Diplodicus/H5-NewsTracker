08ed2ca6057b571d1fd2b2b879115842a5a47324
TjVirb <60270412+TjVirb@users.noreply.github.com>
2020-03-17 14:51:53 -0600

    rss testing

--- tests/testRss.py
+++ tests/testRss.py
@@ -1,8 +1,11 @@
 import unittest
+from unittest.mock import patch
 from H5_News_Tracker.parser import rss
 
 
 class TestRss(unittest.TestCase):
 
     def test_parse_feed(self):
-        pass
+        with patch('H5_News_Tracker.parser.rss.parse_feed.feedparser.parse') as mocked_feed:
+            mocked_feed.return_value = 'title'
+            self.assertIsNotNone(rss.parse_feed())
