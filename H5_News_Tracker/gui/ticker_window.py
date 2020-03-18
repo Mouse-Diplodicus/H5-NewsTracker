c9dc1fe0e53a2eae10ec2ecc70cf63a273e9cc21
MeeH2 <saithach@hotmail.com>
2020-03-17 18:38:28 -0600

    added a change background label

--- H5_News_Tracker/gui/ticker_window.py
+++ H5_News_Tracker/gui/ticker_window.py
@@ -9,13 +9,16 @@ from tkinter import ttk
 class TickerWindow:
 
     root = tkinter.Tk()
+    menu = ttk.Label(root)
     label_ticker = ttk.Label(root)
     button_exit = ttk.Button(root)
+    root.title("New JRRS")
 
     def __init__(self, master=None, **kw):
         """Initializes the display window for the news ticker"""
         print("constructing gui")
-        self.root.overrideredirect(1)
+        self.root.overrideredirect(0)
+        self.menu.configure(text="Change Background")
         self.label_ticker.configure(width=8)
         self.button_exit.configure(text="X", padding=[4, 0, 4, 0], command=self.root.quit)
         self.set_style()
@@ -36,12 +39,14 @@ class TickerWindow:
         style.configure("BR.TLabel", foreground="#000000", background="#931113")
         self.label_ticker.configure(style="WB.TLabel")
         self.button_exit.configure(style="BR.TLabel")
+        self.menu.configure(style="WB.TLabel")
 
     def build(self):
         """Sets organization for label and exit button"""
         print("organizing gui layout")
-        self.label_ticker.grid(row=0, column=0)
-        self.button_exit.grid(row=0, column=1)
+        self.menu.grid(row=0, column=0)
+        self.label_ticker.grid(row=10, column=0)
+        self.button_exit.grid(row=10, column=1)
 
     def update(self, headline, url):
         print("updating ticker to headline: ", headline, "   url: ", url)
