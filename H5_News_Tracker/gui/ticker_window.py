386ba9aa648ae61bae14ee10bd7206c45aa664c1
MeeH2 <saithach@hotmail.com>
2020-03-27 12:30:20 -0600

    added Serena's code. Need to figure out why it's not functioning correctly

--- H5_News_Tracker/gui/ticker_window.py
+++ H5_News_Tracker/gui/ticker_window.py
@@ -3,32 +3,70 @@ Program displays a window with text using Tkinter when run.
 """
 import tkinter
 import webbrowser
+from logging import root
 from tkinter import ttk
-from tkinter.ttk import OptionMenu
+from tkinter.ttk import Label
+
+
+def combo_click_bg():
+    if myCombo_bg.get() == 'Red':
+        myLabel = Label(root, text="Text is now red", background="#f44336").pack()
+
+    if myCombo_bg.get() == 'Green':
+        myLabel = Label(root, text="Text is now green", background="#4caf50").pack()
+
+    if myCombo_bg.get() == 'Blue':
+        myLabel = Label(root, text="Text is now Blue", background="#2196f3").pack()
+
+    if myCombo_bg.get() == 'Default':
+        myLabel = Label(root, text="Text is in default", background="#212121").pack()
+
+
+def combo_click_text():
+    if myCombo_text.get() == 'Red':
+        myLabel = Label(root, text="Text is now red", foreground="#f44336").pack()
+
+    if myCombo_text.get() == 'Green':
+        myLabel = Label(root, text="Text is now green", foreground="#4caf50").pack()
+
+    if myCombo_text.get() == 'Blue':
+        myLabel = Label(root, text="Text is now Blue", foreground="#2196f3").pack()
+
+    if myCombo_text.get() == 'Default':
+        myLabel = Label(root, text="Text is in default", foreground="#212121").pack()
+
+
+text_options = [
+    "Default",
+    "Red",
+    "Green",
+    "Blue",
+]
+
+myCombo_text = ttk.Combobox(root, value=text_options)
+myCombo_text.current()  # shows the current value
+myCombo_text.bind("<<ComboboxSelected>>", combo_click_text)
+myCombo_text.pack()
+
+
+bg_options = [
+    "Default",
+    "Red",
+    "Green",
+    "Blue",
+]
+
+myCombo_bg = ttk.Combobox(root, value=bg_options)
+myCombo_bg.current()  # shows the current value
+myCombo_bg.bind("<<ComboboxSelected>>", combo_click_bg)
+myCombo_bg.pack()
 
 
 class TickerWindow:
     root = tkinter.Tk()
-
-    var = tkinter.StringVar(root)
-    var.set('Select')
-    choices = ['Background Options', 'Black', 'White', 'Blue', 'Pink']
-
     label_ticker = ttk.Label(root)
     button_exit = ttk.Button(root)
     root.title("New JRRS")
-
-    def background(self, select):
-        if select == 'Black':
-            self.menu.configure(background='black')
-        elif select == 'White':
-            self.menu.configure(background='white')
-        elif select == 'Blue':
-            self.menu.configure(background='blue')
-        elif select == 'Pink':
-            self.menu.configure(background='pink')
-
-    menu = OptionMenu(root, var, *choices, command=background)
 
     def __init__(self, master=None, **kw):
         """Initializes the display window for the news ticker"""
@@ -49,19 +87,19 @@ class TickerWindow:
         """Sets styling for various Tkinter objects"""
         print("setting styling")
         style = ttk.Style()
+
         style.configure("default.TLabel", foreground="#000000", background="#ffffff")
         style.configure("WB.TLabel", foreground="#ffffff", background="#000000")
         style.configure("BR.TLabel", foreground="#000000", background="#931113")
         # self.label_ticker.configure(style="WB.TLabel")
         self.button_exit.configure(style="BR.TLabel")
         # self.menu.configure(style="WB.TLabel")
-        if self.choices == 'Black':
-            style.configure(background='black')
 
     def build(self):
         """Sets organization for label and exit button"""
         print("organizing gui layout")
-        self.menu.grid(row=10, column=0)
+        combo_click_bg().grid(row=10, column=0)
+        combo_click_text().grid(row=10, column=2)
         self.label_ticker.grid(row=0, column=0)
         self.button_exit.grid(row=0, column=10)
 
