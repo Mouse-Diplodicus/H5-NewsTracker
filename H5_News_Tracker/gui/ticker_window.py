075272f5da9a02728b53aa06dd8ad6f3b0cc78dd
MeeH2 <saithach@hotmail.com>
2020-03-27 20:47:20 -0600

    Drop down menu works, but not changing background, it's giving an Event AttributeError

--- H5_News_Tracker/gui/ticker_window.py
+++ H5_News_Tracker/gui/ticker_window.py
@@ -3,63 +3,7 @@ Program displays a window with text using Tkinter when run.
 """
 import tkinter
 import webbrowser
-from logging import root
 from tkinter import ttk
-from tkinter.ttk import Label
-
-
-def combo_click_bg():
-    if myCombo_bg.get() == 'Red':
-        myLabel = Label(root, text="Text is now red", background="#f44336").pack()
-
-    if myCombo_bg.get() == 'Green':
-        myLabel = Label(root, text="Text is now green", background="#4caf50").pack()
-
-    if myCombo_bg.get() == 'Blue':
-        myLabel = Label(root, text="Text is now Blue", background="#2196f3").pack()
-
-    if myCombo_bg.get() == 'Default':
-        myLabel = Label(root, text="Text is in default", background="#212121").pack()
-
-
-def combo_click_text():
-    if myCombo_text.get() == 'Red':
-        myLabel = Label(root, text="Text is now red", foreground="#f44336").pack()
-
-    if myCombo_text.get() == 'Green':
-        myLabel = Label(root, text="Text is now green", foreground="#4caf50").pack()
-
-    if myCombo_text.get() == 'Blue':
-        myLabel = Label(root, text="Text is now Blue", foreground="#2196f3").pack()
-
-    if myCombo_text.get() == 'Default':
-        myLabel = Label(root, text="Text is in default", foreground="#212121").pack()
-
-
-text_options = [
-    "Default",
-    "Red",
-    "Green",
-    "Blue",
-]
-
-myCombo_text = ttk.Combobox(root, value=text_options)
-myCombo_text.current()  # shows the current value
-myCombo_text.bind("<<ComboboxSelected>>", combo_click_text)
-myCombo_text.pack()
-
-
-bg_options = [
-    "Default",
-    "Red",
-    "Green",
-    "Blue",
-]
-
-myCombo_bg = ttk.Combobox(root, value=bg_options)
-myCombo_bg.current()  # shows the current value
-myCombo_bg.bind("<<ComboboxSelected>>", combo_click_bg)
-myCombo_bg.pack()
 
 
 class TickerWindow:
@@ -67,6 +11,13 @@ class TickerWindow:
     label_ticker = ttk.Label(root)
     button_exit = ttk.Button(root)
     root.title("New JRRS")
+
+    options = [
+        "Default",
+        "Red",
+        "Green",
+        "Blue",
+    ]
 
     def __init__(self, master=None, **kw):
         """Initializes the display window for the news ticker"""
@@ -91,15 +42,39 @@ class TickerWindow:
         style.configure("default.TLabel", foreground="#000000", background="#ffffff")
         style.configure("WB.TLabel", foreground="#ffffff", background="#000000")
         style.configure("BR.TLabel", foreground="#000000", background="#931113")
+        style.configure("RBg.TLabel", background="#f44336")
+        # style.configure("RT.TLabel", foreground="#f44336")
+        style.configure("GBg.TLabel", background="#4caf50")
+        # style.configure("GT.TLabel", foreground="#4caf50")
+        style.configure("BBg.TLabel", background="#2196f3")
+        # style.configure("BT.TLabel", foreground="#2196f3")
         # self.label_ticker.configure(style="WB.TLabel")
         self.button_exit.configure(style="BR.TLabel")
         # self.menu.configure(style="WB.TLabel")
 
+    def combo_click_bg(self):
+        if self.combo_click_bg().get() == 'Red':
+            self.label_ticker.configure(style="RBg.TLabel")
+
+        elif self.combo_click_bg().get() == 'Green':
+            self.label_ticker.configure(style="GBg.TLabel")
+
+        elif self.combo_click_bg().get() == 'Blue':
+            self.label_ticker.configure(style="BBg.TLabel")
+
+        elif self.combo_click_bg().get() == 'Default':
+            self.label_ticker.configure(style="default.TLabel")
+
+    myCombo_bg = ttk.Combobox(root, value=options)
+    myCombo_bg.current()  # shows the current value
+
+    myCombo_bg.bind("<<ComboboxSelected>>", combo_click_bg)
+    myCombo_bg.pack()
+
     def build(self):
         """Sets organization for label and exit button"""
         print("organizing gui layout")
-        combo_click_bg().grid(row=10, column=0)
-        combo_click_text().grid(row=10, column=2)
+        self.myCombo_bg.grid(row=10, column=0)
         self.label_ticker.grid(row=0, column=0)
         self.button_exit.grid(row=0, column=10)
 
