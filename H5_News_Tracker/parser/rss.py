35d90481fddf90209e64bfc5b451b6d0d86f9a37
Matthew <williamm1.matt@yahoo.com>
2020-03-28 12:46:58 -0600

    cleaning up

--- H5_News_Tracker/parser/rss.py
+++
@@ -1,7 +0,0 @@
-import feedparser
-
-
-def parse_feed(index):
-    feed = feedparser.parse('https://news.google.com/news/rss')
-    # feed_show = {'entries': [{feed['entries'][index]['title']}]}
-    return feed['entries'][index]['title']
