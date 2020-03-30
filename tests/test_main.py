e8be2297f16678e2702b06c8165b56cac54f377e
TjVirb <60270412+TjVirb@users.noreply.github.com>
2020-03-29 23:11:47 -0600

    finish all build_rss_ticker tests except threading tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,10 @@
 import unittest
 import threading
 import time
-import feedparser
 from unittest.mock import MagicMock
 from unittest.mock import patch
 from H5_News_Tracker.controller import main
+from H5_News_Tracker.gui import ticker_window
 
 
 class TestMain(unittest.TestCase):
@@ -13,10 +13,15 @@ class TestMain(unittest.TestCase):
     def test_build_rss_ticker(self):
 
         with patch('H5_News_Tracker.controller.main.pull_feed') as mocked_pull_feed:
-            main.build_rss_ticker()
-            mocked_pull_feed.assert_called_with('https://news.google.com/news/rss')
-
-
+            with patch('H5_News_Tracker.controller.main.build_library') as mocked_build_library:
+                with patch('H5_News_Tracker.gui.ticker_window.TickerWindow') as mocked_ticker_window:
+                    with patch('threading.Thread') as mocked_thread:
+                        main.build_rss_ticker()
+                        mocked_pull_feed.assert_called_with('https://news.google.com/news/rss')
+                        mocked_build_library.assert_called_with(mocked_pull_feed('https://news.google.com/news/rss'))
+                        mocked_ticker_window.assert_any_call()
+                        # mocked_thread.assert_any_call()
+                        mocked_ticker_window.assert_has_calls(ticker_window.TickerWindow.start())
 
     def test_build_library(self):
         item = MagicMock()
