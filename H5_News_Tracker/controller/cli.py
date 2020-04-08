278c16692996190b4b467d42451fa0db578310db
gtena <gtena@msudenver.edu>
2020-04-07 20:47:09 -0600

    more testing

--- H5_News_Tracker/controller/cli.py
+++ H5_News_Tracker/controller/cli.py
@@ -1,15 +1,19 @@
-""" Command Line Interface for H5 NewsTracker """
+"""
+Command Line Interface for H5 NewsTracker
+"""
 import argparse
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="H5 command line interface")
-    parser.add_argument('--url', action='store', dest='url',default="",
-                        help="enter a RSS url", nargs='*')
-    parser.add_argument('--file', action='store', dest='file', default="",
-                        help="please enter a file name", nargs='*')
-    parser.add_argument('--config', action='store', dest='config', default="",
-                        help="enter .yaml file to configure", nargs='*')
-    #print(parser.parse_args(['--url', '--file', '--config']))
 
-    args = parser.parse_args()
-    return args
+class CmdArgs:
+
+    def parse_args(args):
+        parser = argparse.ArgumentParser(description="H5 command line interface")
+        parser.add_argument('--url', action='store', dest='url', default="",
+                            help="enter a RSS url", nargs='*')
+        parser.add_argument('--file', action='store', dest='file', default="",
+                            help="please enter a file name", nargs='*')
+        parser.add_argument('--config', action='store', dest='config', default="",
+                            help="enter .yaml file to configure", nargs='*')
+
+        #args = parser.parse_args(args)
+        return parser.parse_args(args)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,17 +1,21 @@
-""" Tests for Command Line Interface """
+"""
+Tests for Command Line Interface
+"""
 import unittest
-from unittest.mock import MagicMock
-from unittest.mock import patch
 from H5_News_Tracker.controller import cli
+
+
+
+def parse_args(self):
+    pass
 
 
 class TestCli(unittest.TestCase):
 
     def test_parse_args(self):
-        #cli = MagicMock()
-        #parsed = cli.parse_args(['--url', 'test'])
-        #self.assertEqual(parsed.parse_args, 'test')
-        pass
+        parser = parse_args(['--url'])
+        self.assertTrue(parser.url)
+
 
 if __name__ == '__main__':
     unittest.main()
