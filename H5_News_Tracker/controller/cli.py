8f2bfe502c99dea219d12e69274cde2960a41d87
gtena <gtena@msudenver.edu>
2020-04-07 22:06:22 -0600

    fixed testing

--- H5_News_Tracker/controller/cli.py
+++ H5_News_Tracker/controller/cli.py
@@ -4,16 +4,14 @@ Command Line Interface for H5 NewsTracker
 import argparse
 
 
-class CmdArgs:
-
-    def parse_args(args):
-        parser = argparse.ArgumentParser(description="H5 command line interface")
-        parser.add_argument('--url', action='store', dest='url', default="",
-                            help="enter a RSS url", nargs='*')
-        parser.add_argument('--file', action='store', dest='file', default="",
-                            help="please enter a file name", nargs='*')
-        parser.add_argument('--config', action='store', dest='config', default="",
-                            help="enter .yaml file to configure", nargs='*')
+def parse_args(args):
+    parser = argparse.ArgumentParser(description="H5 command line interface")
+    parser.add_argument('--url', dest='url', action='store', default="",
+                        help="enter a RSS url", nargs='*')
+    parser.add_argument('--file', action='store', dest='file', default="",
+                        help="please enter a file name", nargs='*')
+    parser.add_argument('--config', action='store', dest='config', default="",
+                        help="enter .yaml file to configure", nargs='*')
 
         #args = parser.parse_args(args)
-        return parser.parse_args(args)
+    return parser.parse_args(args)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,19 +2,23 @@
 Tests for Command Line Interface
 """
 import unittest
+from unittest.mock import MagicMock
 from H5_News_Tracker.controller import cli
-
-
-
-def parse_args(self):
-    pass
 
 
 class TestCli(unittest.TestCase):
 
+    def test_parse_args_default(self):
+        parser = cli.parse_args([])
+        self.assertFalse(parser.url)
+
     def test_parse_args(self):
-        parser = parse_args(['--url'])
-        self.assertTrue(parser.url)
+        parser = cli.parse_args(['--url'])
+        self.assertTrue(self, parser.url)
+        parser = cli.parse_args(['--file'])
+        self.assertTrue(self, parser.file)
+        parser = cli.parse_args(['--config'])
+        self.assertTrue(self, parser.config)
 
 
 if __name__ == '__main__':
