6fd0893c0cb95a564de72722109ea4f1b808b926
Serena Zarate <serena.zarate@gmail.com>
2020-04-08 16:34:13 -0600

    Removed titlebar and kept the edit menu but no functionality yet

--- H5_News_Tracker/gui/ticker_window.py
+++ H5_News_Tracker/gui/ticker_window.py
@@ -5,12 +5,23 @@ import tkinter
 import tkinter as tk
 import webbrowser
 from tkinter import ttk
+from tkinter.ttk import OptionMenu
 
 
 class TickerWindow:
     root = tkinter.Tk()
+    root.overrideredirect(True)  # Delete border
 
-    menubar = tk.Menu(root)
+    menu_bar = tk.Menu(root)
+    textColor = tkinter.StringVar()
+    textColor.set('black')
+    background = tkinter.StringVar()
+    background.set('white')
+
+
+    var = tkinter.StringVar(root)
+    # var.set('Select')
+    choices = ['Background Options', 'Black', 'White', 'Blue', 'Pink']
 
     label_ticker = ttk.Label(root)
     button_exit = ttk.Button(root)
@@ -19,10 +30,10 @@ class TickerWindow:
     def __init__(self, master=None, **kw):
         """Initializes the display window for the news ticker"""
         print("constructing gui")
-        self.root.overrideredirect(0)
-        self.label_ticker.configure(width=8)
-        self.button_exit.configure(text="X", padding=[4, 0, 4, 0], command=self.root.quit)
-        self.set_style()
+        self.root.overrideredirect(1)
+        self.label_ticker.configure(width=8, background='#000000', foreground='#ffffff')
+        self.button_exit.configure(text="X", padding=[2, 0, 2, 0], command=self.root.quit)
+        # self.set_style()
         self.build()
         print("Gui constructed")
 
@@ -40,11 +51,26 @@ class TickerWindow:
         style.configure("BR.TLabel", foreground="#000000", background="#931113")
         # self.label_ticker.configure(style="WB.TLabel")
         self.button_exit.configure(style="BR.TLabel")
-        # self.menu.configure(style="WB.TLabel")
+        # self.menu.configure(style=" WB.TLabel")
+        # if self.choices == 'Black':
+        #     style.configure(background='black')
+
+    # def background(self, menu, label_ticker):
+    #     if menu == 'Black':
+    #         ttk.Label(style='WB.TLabel')
+    #     elif menu == 'White':
+    #         label_ticker.configure(background='white')
+    #     elif menu == 'Blue':
+    #         label_ticker.configure(background='blue')
+    #     elif menu == 'Pink':
+    #         label_ticker.configure(background='pink')
+    #
+    # menu = OptionMenu(root, var, *choices, command=background)
 
     def build(self):
         """Sets organization for label and exit button"""
         print("organizing gui layout")
+        # self.menu.grid(row=10, column=0)
         self.label_ticker.grid(row=0, column=0)
         self.button_exit.grid(row=0, column=10)
 
@@ -53,10 +79,21 @@ class TickerWindow:
         self.label_ticker.configure(text=headline, width=len(headline))
         self.label_ticker.bind("<Button-1>", lambda e: webbrowser.open_new(url))
 
-    edit_menu = tk.Menu(menubar, tearoff=0, foreground="black")
-    edit_menu.add_cascade(label="Text settings")
+    edit_menu = tk.Menu(menu_bar)
 
-    edit_menu.add_cascade(label="Background settings")
-    menubar.add_cascade(menu=edit_menu, label="Edit")
+    submenu = tk.Menu(edit_menu)
+    submenu.add_command(label="Default")
+    submenu.add_command(label="Red")
+    submenu.add_command(label="Blue")
+    edit_menu.add_cascade(label="Text settings", menu=submenu)
 
-    root.config(menu=menubar)
+    submenu2 = tk.Menu(edit_menu)
+    submenu2.add_command(label="Black")
+    submenu2.add_command(label="White")
+    submenu2.add_command(label="Pink")
+    submenu2.add_command(label="Blue")
+    edit_menu.add_cascade(label="Background settings", menu=submenu2)
+
+    menu_bar.add_cascade(menu=edit_menu, label="Edit")
+
+    root.config(menu=menu_bar)
